36d961d5286818d8336b9cdf3a10927cca40ea75
kylelainez <kyleadrianlainez@gmail.com>
2021-06-15 21:02:34 -0700

    Auto Send Message

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-
+let channel;
+let minuteInterval;
 const event = require('./events');
 
 const messages = [
@@ -12,6 +13,15 @@ const messages = [
     'Dazai Gehy s39',
 ];
 
+client.on('ready', async () => {
+    channel = await client.channels.fetch('854564667321483304');
+    minuteInterval = setInterval(() => {
+        if (new Date().getUTCMinutes() === 0) {
+            startTimer();
+        }
+    }, 60000);
+});
+
 client.on('message', (msg) => {
     if (msg.content === '!events') {
         msg.channel.send(event.getEvents(msg.createdTimestamp));
@@ -20,6 +30,23 @@ client.on('message', (msg) => {
     if (msg.content === '!gehy') {
         msg.channel.send(messages[Math.floor(Math.random() * 4)]);
     }
+
+    if (msg.content === '!test') {
+        msg.channel.send(`Minutes: ${new Date().getUTCMinutes()}`);
+        console.log();
+    }
 });
+
+function sendMessage() {
+    channel.send(event.getEvents(new Date()));
+}
+
+function startTimer() {
+    clearInterval(minuteInterval);
+    sendMessage();
+    const hourInterval = setInterval(() => {
+        sendMessage();
+    }, 1000 * 60 * 60);
+}
 
 client.login(process.env.client);
